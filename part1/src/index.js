57e56da0b8dd9c30b9ba7bdc6a9f252bd370f623
Anthony Morganelli <morganellia@wit.edu>
2020-04-07 20:22:16 -0400

    1.2: course information, step2

--- part1/src/index.js
+++ part1/src/index.js
@@ -11,27 +11,27 @@ const Header = (props) => {
 
 const Content = (props) => {
   return(
-    <>
-      <p>
-       {props.part1} {props.exercises1}
-     </p>
+    <div>
+      <Part name={props.part1} number={props.exercises1}/>
+      <Part name={props.part2} number={props.exercises2}/>
+      <Part name={props.part3} number={props.exercises3}/>
+    </div>
+  )
+}
 
-      <p>
-       {props.part2} {props.exercises2}
-      </p>
-
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-    </>
+const Part = (props) => {
+  return(
+    <div>
+      <p>{props.name} {props.number}</p>
+    </div>
   )
 }
 
 const Total = (props) => {
   return (
-    <>
+    <div>
       <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    </>
+    </div>
   )
 }
 
