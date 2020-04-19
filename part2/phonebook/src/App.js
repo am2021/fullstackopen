3bfcf6d167049893adb479e7817c9c1d682844a0
Anthony Morganelli <morganellia@wit.edu>
2020-04-19 16:14:15 -0400

    added filtering capabilities

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,10 +3,14 @@ import Contact from './components/Contact'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -16,6 +20,11 @@ const App = () => {
   const handleNumberChange = (event) => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setFilter(event.target.value)
   }
 
   const addContact = (event) => {
@@ -35,9 +44,16 @@ const App = () => {
     }
   }
 
+  const filteredPersons = (filter === '')
+    ? persons
+    : persons.filter(person => new RegExp(filter, 'i').test(person.name))
+ 
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input onKeyUp={handleFilterChange}/></div>
+      <h2>add a new</h2>
       <form onSubmit={addContact}>
         <div>
           name: <input 
@@ -57,7 +73,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => 
+        {filteredPersons.map((person) => 
           <Contact key={person.name} name={person.name} number={person.number}/>
         )}
       </ul>
